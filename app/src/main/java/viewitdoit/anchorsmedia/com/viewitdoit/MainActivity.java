bd68f023819e98839f8b505c1d381ecb721964be
John Woodcock <john@userx.co>
2017-04-05 10:41:44 -0400

    initial commit

---
+++ app/src/main/java/viewitdoit/anchorsmedia/com/viewitdoit/MainActivity.java
@@ -0,0 +1,13 @@
+package viewitdoit.anchorsmedia.com.viewitdoit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
